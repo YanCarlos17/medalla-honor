2bde1b738cf8541e20dd0b19d36b82717e8bd660
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-05-07 13:48:28 -0500

    se empiezan los cambios en los estilos de la vista del Ranking

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -7,16 +7,21 @@
 <div class="container">
     
     
-    <header class="header">
-        <div class="medals">
-            <img src="" alt="" class="medal medal-gold">
-            <img src="" alt="" class="medal medal-silver">
-            <img src="" alt="" class="medal medal-bronze">
+    <header class="header header2">
+        <div class="header2__left">
+            <p class="header2__parraph">
+                Medalla de Honor <span>2018 <br>
+                Premio a la excelencia</span>
+            </p>
+            <h1 class="header2__title">Ranking</h1>
+        </div>
+        <div class="header2__right">
+            <img class="header2__medals" src="{{asset('../public/img/medals.png')}}" alt="">
         </div>
     </header>
 
     <main>
-        <h1>Ranking</h1>
+        
         <div class="panels">
             <input class="panel__trigger" type="radio" value="nacional" name="ranking" id="nacional" checked><label for="nacional">Nacional</label>
             <input class="panel__trigger" type="radio" value="especializacion" name="ranking" id="especializacion"><label for="especializacion">Especialización</label>
@@ -25,8 +30,20 @@
                 <div class="podiums">
                 @foreach($nationaltop as $index => $top)
                     <div class="podium">
+                        <span class="podium__position">#{{ $index }}</span>
+                        <img class="podium__img" src="{{asset('../public/img/oro.png')}}" alt="">
+                        <h3 class="podium__name">{{ $top->name }}</h3>
+                        <p class="podium__score">{{ $top->total_score }} pts</p>
+                    </div>
+                    <div class="podium">
                         <span class="podium__position">#{{ $index+1 }}</span>
-                        <img class="podium__img" src="" alt="">
+                        <img class="podium__img" src="{{asset('../public/img/plata.png')}}" alt="">
+                        <h3 class="podium__name">{{ $top->name }}</h3>
+                        <p class="podium__score">{{ $top->total_score }} pts</p>
+                    </div>
+                    <div class="podium">
+                        <span class="podium__position">#{{ $index+2 }}</span>
+                        <img class="podium__img" src="{{asset('../public/img/bronce.png')}}" alt="">
                         <h3 class="podium__name">{{ $top->name }}</h3>
                         <p class="podium__score">{{ $top->total_score }} pts</p>
                     </div>
@@ -57,7 +74,7 @@
                         @foreach($storetop as $index => $top)
                             <div class="podium">
                                 <span class="podium__position">#{{ $index+1 }}</span>
-                                <img class="podium__img" src="" alt="">
+                                <img class="podium__img" src="{{asset('../public/img/oro.png')}}" alt="">
                                 <h3 class="podium__name">{{ $top->name }}</h3>
                                 <p class="podium__score">{{ $top->score }} pts</p>
                             </div>
@@ -78,12 +95,13 @@
                         @endforeach
                         </table>  
                     </div>
+
                     <div class="panel" id="mayoristas-content">
                         <div class="podiums">
                         @foreach($wholesalertop as $index => $top)
                             <div class="podium">
                                 <span class="podium__position">#{{ $index+1 }}</span>
-                                <img class="podium__img" src="" alt="">
+                                <img class="podium__img" src="{{asset('../public/img/plata.png')}}" alt="">
                                 <h3 class="podium__name">{{ $top->name }}</h3>
                                 <p class="podium__score">{{ $top->score }} pts</p>
                             </div>
@@ -104,12 +122,25 @@
                         @endforeach
                         </table>
                     </div>
+
                     <div class="panel" id="supermercados-content">
                     <div class="podiums">
                         @foreach($supermarkettop as $index => $top)
                             <div class="podium">
+                                <span class="podium__position">#{{ $index }}</span>
+                                <img class="podium__img" src="{{asset('../public/img/oro.png')}}" alt="">
+                                <h3 class="podium__name">{{ $top->name }}</h3>
+                                <p class="podium__score">{{ $top->score }} pts</p>
+                            </div>
+                            <div class="podium">
                                 <span class="podium__position">#{{ $index+1 }}</span>
-                                <img class="podium__img" src="" alt="">
+                                <img class="podium__img" src="{{asset('../public/img/plata.png')}}" alt="">
+                                <h3 class="podium__name">{{ $top->name }}</h3>
+                                <p class="podium__score">{{ $top->score }} pts</p>
+                            </div>
+                            <div class="podium">
+                                <span class="podium__position">#{{ $index+2 }}</span>
+                                <img class="podium__img" src="{{asset('../public/img/bronce.png')}}" alt="">
                                 <h3 class="podium__name">{{ $top->name }}</h3>
                                 <p class="podium__score">{{ $top->score }} pts</p>
                             </div>
